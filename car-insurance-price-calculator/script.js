1afed960a3c807349ab48a86192e330656bafa19
aman-maharshi <amanm4harshi@gmail.com>
2019-04-09 21:50:51 +0530

    getFormData fuctionality added

--- car-insurance-price-calculator/script.js
+++ car-insurance-price-calculator/script.js
@@ -1,14 +1,58 @@
 //  VARIABLES 
-let yearSelect = document.querySelector("#year"); 
-
+let yearSelect = document.querySelector("#year"),
+    form = document.getElementById('form'),
+    countrySelect = document.getElementById('country');
 //  EVENT LISTENERS
-document.addEventListener('DOMContentLoaded', loadYearSelector)
+// document.addEventListener('DOMContentLoaded', loadYearSelector)
 
-//  FUNCTIONS
+document.addEventListener('DOMContentLoaded', function(){
+    const html = new HTMLUI();
+    html.displayYears();
+});
 
-function loadYearSelector() {
-    for(let i = 2000; i < 2020; i++) {
-        let option = `<option value="${i}">${i}</option>`
-        yearSelect.innerHTML += option;
+form.addEventListener('submit', getFormData)
+
+//  FUNCTIONS AND OBJECTS
+
+// load the year select options of the form
+// function loadYearSelector() {
+//     for(let i = 2000; i < 2020; i++) {
+//         let option = `<option value="${i}">${i}</option>`
+//         yearSelect.innerHTML += option;
+//     }
+// }
+
+function HTMLUI() {}
+
+HTMLUI.prototype.displayYears = function() {
+    const max = new Date().getFullYear(),
+          min = max - 20;
+    
+    for(let i = max; i >= min; i--) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        yearSelect.appendChild(option);
     }
+}
+
+function getFormData(e) {
+    e.preventDefault();
+    
+    // can't declare globally, as we have to get the value at the time form is submitted
+    let insuranceTypeSelect = document.querySelector('input[name="type"]:checked');
+
+    const country = countrySelect.value,
+          year = yearSelect.value,
+          insuranceType = insuranceTypeSelect.value;
+    // console.log(country, year, insuranceType);
+
+    if (country === '' || year === '' || insuranceType === '' ) {
+        document.getElementById('result').textContent = 'Invalid Input';
+    }
+    else {
+        document.getElementById('result').textContent = '';
+        console.log('Input Submitted');
+    }
+
 }
