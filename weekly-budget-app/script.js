4cd8669df7b7d2a10b1338d751148b8c04b722e6
aman-maharshi <amanm4harshi@gmail.com>
2019-04-15 13:48:49 +0530

    first column styling complete

---
+++ weekly-budget-app/script.js
@@ -0,0 +1,9 @@
+addExpence = document.getElementById('add-expence');
+// console.log(addExpence)
+
+addExpence.addEventListener('submit', demo);
+function demo (event) {
+    event.preventDefault();
+
+    // console.log(2)
+}
