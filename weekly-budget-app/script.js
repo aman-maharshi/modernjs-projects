795173ec339c11e1dd549f46bacc5bbc72164c2b
aman-maharshi <amanm4harshi@gmail.com>
2019-04-15 15:38:42 +0530

    added init and formSubmit functions

--- weekly-budget-app/script.js
+++ weekly-budget-app/script.js
@@ -1,8 +1,32 @@
-addExpence = document.getElementById('add-expence');
-// console.log(addExpence)
 
-addExpence.addEventListener('submit', demo);
-function demo (event) {
+//  VARIABLES
+const addExpence = document.getElementById('add-expence'),
+    inputExpence = document.getElementById('expence'),
+    inputAmount = document.getElementById('amount'),
+    totalBudget = document.getElementById('total-budget'),
+    leftBudget = document.getElementById('left-budget');
+
+let userBudget;
+
+//  EVENT LISTENERS
+document.addEventListener('DOMContentLoaded', init);
+addExpence.addEventListener('submit', formSubmit);
+
+
+//  CLASSES
+
+// FUNCTIONS
+function init() {
+    userBudget = prompt('What\'s your budget for the week?');
+    if(userBudget === null|| userBudget === ''|| userBudget ==='0') {
+        window.location.reload();
+    }
+    else {
+        totalBudget.textContent = Number(userBudget);
+    }
+
+}
+function formSubmit (event) {
     event.preventDefault();
-    console.log('It Works!!')
+    console.log('You spent: '+inputAmount.value+' ₹ on '+inputExpence.value);
 }
