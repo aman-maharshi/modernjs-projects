820e704b2568cca18c459283a711f5ec74a8a2ed
aman-maharshi <amanm4harshi@gmail.com>
2019-04-30 11:26:53 +0530

    id's modified

---
+++ weekly-budget-app-v2/script.js
@@ -0,0 +1,7 @@
+let form = document.querySelector('#form'),
+    name = document.querySelector('#name'),
+    amount = document.querySelector('#amount'),
+    message = document.querySelector('#message'),
+    list = document.querySelector('#list'),
+    budget = document.querySelector('#budget'),
+    left = document.querySelector('#left');
