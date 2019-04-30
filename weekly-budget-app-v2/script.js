2b79495d41573bcda64fe02cc69ddb800078a7a6
aman-maharshi <amanm4harshi@gmail.com>
2019-04-30 16:07:00 +0530

    budget input changed and init function added

--- weekly-budget-app-v2/script.js
+++ weekly-budget-app-v2/script.js
@@ -1,7 +1,47 @@
-let form = document.querySelector('#form'),
-    name = document.querySelector('#name'),
-    amount = document.querySelector('#amount'),
-    message = document.querySelector('#message'),
-    list = document.querySelector('#list'),
-    budget = document.querySelector('#budget'),
-    left = document.querySelector('#left');
+//  VARIABLES
+const enterBudget = document.querySelector('#enter-budget'),
+    formFirst = document.querySelector('#form-first'),
+    expenceName = document.querySelector('#name'),
+    expenceAmount = document.querySelector('#amount'),
+    formSecond = document.querySelector('#form-second');
+
+let budget;
+
+
+//  EVENT LISTENERS
+document.addEventListener('DOMContentLoaded', function() {
+    enterBudget.focus();
+})
+formFirst.addEventListener('submit', init);
+formSecond.addEventListener('submit', addExpence);
+
+
+//  FUNCTIONS
+function init(e) {  
+    e.preventDefault();
+    budget = enterBudget.value;
+    if (budget !== '') {
+        enableForm();
+        expenceName.focus();
+    }
+    else {
+        document.getElementById('message-first').textContent = 'This field cant\'t be left empty';
+        setTimeout(function() {
+            document.getElementById('message-first').style.display = 'none';
+        }, 2000)
+    }
+}
+function enableForm() {
+    expenceName.disabled = false;
+    expenceAmount.disabled = false;
+    document.getElementById('add-btn').disabled = false;
+}
+function addExpence() {
+
+}
+function addToList() {
+
+}
+function updateBudget() {
+
+}
