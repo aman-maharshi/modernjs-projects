5df6ab53fdef3064621439979570dc7a1a3e2f78
aman-maharshi <amanm4harshi@gmail.com>
2019-05-02 21:53:56 +0530

    load from json complete

--- examples/Ajax/script.js
+++ examples/Ajax/script.js
@@ -1,3 +1,6 @@
+/*
+    FIRST
+*/
 document.getElementById('btn1').addEventListener('click', loadData);
 
 function loadData() {
@@ -19,7 +22,7 @@ function loadData() {
         if(this.status === 200) {
             let obj = this.responseText;
             obj = JSON.parse(obj);
-            document.getElementById('result1').innerHTML  = `<p>Location: ${obj.city}</p>`;
+            document.getElementById('result1').innerHTML  = `<p class="crimson">Location: ${obj.city}</p>`;
         }
     }
     // STEP 3 another way of executing ajax call
@@ -39,5 +42,34 @@ function loadData() {
 
 
     // STEP 4: send request
+    xhr.send();
+}
+
+/*
+    SECOND
+*/
+document.getElementById('btn2').addEventListener('click', loadPlayerInfo);
+
+function loadPlayerInfo() {
+
+    // 1. create object
+    const xhr = new XMLHttpRequest();
+    // 2. open connnection
+    xhr.open('GET', 'players.json', true);
+    // 3. execute function
+    xhr.onload = function() {
+        if(this.status === 200) {
+            const players = JSON.parse(this.responseText);
+            let output = '';
+            players.forEach(element => {
+                output += `<p class="crimson">${element.player} with ${element.titles} titles </p>`;
+            });
+            console.log(typeof(output))
+            output += `<a href="players.json">players.json<a>`
+            console.log(output)
+            document.getElementById('result2').innerHTML = output;
+        }
+    }
+    // 4. send request
     xhr.send();
 }
