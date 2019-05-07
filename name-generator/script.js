7e35acecc2db702205abc2258dc42878ba857fd5
aman-maharshi <amanm4harshi@gmail.com>
2019-05-08 00:26:10 +0530

    ajaxRequest() added

--- name-generator/script.js
+++ name-generator/script.js
@@ -12,16 +12,30 @@ function laodNames(e) {
     if (region != '' && amount != '') {
         url +=  `region=${region}`;
         url += `&gender=${gender}`;
-        url += `&amount=${amount}`;
-        console.log(url);    
+        url += `&amount=${amount}`;  
+        ajaxRequest(url);
     }
     if (amount == '') {
         document.querySelector('.warning').textContent = 'All fields are mandatory';
         setTimeout(function() {
             document.querySelector('.warning').textContent = '';
         }, 2000)
-        
     }
-    
+}
 
+function ajaxRequest(url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function() {
+        if(this.status == 200) {
+            let obj = JSON.parse(this.responseText);
+            let output = '';
+            obj.forEach(element => {
+                output += `<li>${element.name}</li>`
+            });
+            document.getElementById('result').innerHTML = output;
+
+        }
+    }
+    xhr.send();
 }
