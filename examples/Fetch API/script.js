ba5613bbb9c8f89b837391e0678ae3415b83680a
aman-maharshi <amanm4harshi@gmail.com>
2019-05-09 12:43:28 +0530

    load data functionality complete

---
+++ examples/Fetch API/script.js
@@ -0,0 +1,29 @@
+const btn1 = document.getElementById('btn1'),
+      btn2 = document.getElementById('btn2'),
+      btn3 = document.getElementById('btn3'),
+      result = document.getElementById('result');
+
+btn1.addEventListener('click', loadText);
+btn2.addEventListener('click', loadJson);
+btn3.addEventListener('click', loadJsonApi);
+
+function loadText() {
+    // Step 1: fetch the url / file
+    fetch('data.txt')
+    // Step 2: use promise
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(data) {
+        // console.log(data);
+        result.innerHTML = data;
+    })
+}
+
+function loadJson() {
+
+}
+
+function loadJsonApi() {
+
+}
