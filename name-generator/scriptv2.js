5bf42e6c3ee0e07dedf9700f9815b173653bd351
aman-maharshi <amanm4harshi@gmail.com>
2019-05-09 19:30:15 +0530

    xhr changed to fetch api

---
+++ name-generator/scriptv2.js
@@ -0,0 +1,61 @@
+document.getElementById('form').addEventListener('submit', laodNames);
+
+function laodNames(e) {
+    e.preventDefault();
+    const region = document.getElementById('country').value;
+    const gender = document.getElementById('gender').value;
+    const amount = document.getElementById('quantity').value;
+
+    // build URL
+    let url = 'https://uinames.com/api/?';
+    if (region != '' && amount != '') {
+        url +=  `region=${region}`;
+        url += `&gender=${gender}`;
+        url += `&amount=${amount}`;  
+        ajaxRequest(url);
+    }
+    if (amount == '' || region == '') {
+        document.querySelector('.warning').textContent = 'All fields are mandatory';
+        setTimeout(function() {
+            document.querySelector('.warning').textContent = '';
+        }, 2000)
+    }
+}
+
+function ajaxRequest(url) {
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            let output = '<h4>Generated Names</h4>'
+            output += '<ul>'
+            data.forEach(element => {
+                output += `<li>${element.name}</li>`
+            });
+            output += '</ul>'
+            document.getElementById('result').innerHTML = output;
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+}
+
+
+// function ajaxRequest(url) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', url, true);
+//     xhr.onload = function() {
+//         if(this.status == 200) {
+//             const obj = JSON.parse(this.responseText);
+//             let output = '<h4>Generated Names</h4>'
+//             output += '<ul>'
+//             obj.forEach(element => {
+//                 output += `<li>${element.name}</li>`
+//             });
+//             output += '</ul>'
+//             document.getElementById('result').innerHTML = output;
+//         }
+//     }
+//     xhr.send();
+// }
